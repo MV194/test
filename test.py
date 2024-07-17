b66318798e3197b321107a09de1da2eeb036bbd5
MV194 <marko.vulpin@gmail.com>
2024-07-17 10:59:30 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-print("Hello World ! This is a test")
+message = input("Hello World ! This is a test. Give me a message that you would like to share...   ")
+
+print("Here is your message: " + message)
