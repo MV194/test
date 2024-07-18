0b5aac0a98dab93f05a5a1697c7f7b5a83790d27
MV194 <marko.vulpin@gmail.com>
2024-07-18 09:38:07 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 message = input("Hello World ! This is a test. Give me a message that you would like to share...   ")
 
 print("Here is your message: " + message)
+
+print("Now lets play Rock-Paper-Scissors")
+
